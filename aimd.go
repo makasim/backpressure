1374b0ee956f7302d9d89595ddfb2256f79976da
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2022-12-11 15:44:18 +0200

    add stats

--- aimd.go
+++ aimd.go
@@ -3,6 +3,7 @@ package backpressure
 import (
 	"log"
 	"math"
+	"sync"
 	"sync/atomic"
 	"time"
 
@@ -23,6 +24,20 @@ type AIMDConfig struct {
 	LatencyPercentile float64
 }
 
+type AIMDStats struct {
+	Max  int64
+	Used int64
+
+	MaxMax                int64
+	MaxMin                int64
+	SuccessfulCounter     int64
+	CongestedCounter      int64
+	DeniedCounter         int64
+	DecideIncreaseCounter int64
+	DecideDecreaseCounter int64
+	DecideSameCounter     int64
+}
+
 func DefaultAIMDConfig() AIMDConfig {
 	return AIMDConfig{
 		DecideInterval:   time.Second * 5,
@@ -41,16 +56,17 @@ type AIMD struct {
 
 	max        int64
 	used       int64
+	denied     int64
 	successful int64
 	congested  int64
 
-	totalSuccessful int64
-	totalCongested  int64
+	stats    AIMDStats
+	muxStats sync.RWMutex
 
 	h *hdrhistogram.Histogram
 }
 
-func New(cfg AIMDConfig) (*AIMD, error) {
+func NewAIMD(cfg AIMDConfig) (*AIMD, error) {
 
 	if cfg.MaxMax <= 0 {
 		cfg.MaxMax = math.MaxInt
@@ -81,6 +97,7 @@ func (bp *AIMD) Acquire() (Token, bool) {
 	used := atomic.AddInt64(&bp.used, 1)
 	if used > atomic.LoadInt64(&bp.max) {
 		atomic.AddInt64(&bp.used, -1)
+		atomic.AddInt64(&bp.denied, 1)
 		return Token{}, false
 	}
 
@@ -103,7 +120,16 @@ func (bp *AIMD) Release(t Token) {
 	} else {
 		atomic.AddInt64(&bp.congested, 1)
 	}
+}
 
+func (bp *AIMD) Stats() AIMDStats {
+	bp.muxStats.RLock()
+	defer bp.muxStats.RUnlock()
+
+	s := bp.stats
+	s.Used = atomic.LoadInt64(&bp.used)
+
+	return s
 }
 
 func (bp *AIMD) decideLoop() {
@@ -112,20 +138,40 @@ func (bp *AIMD) decideLoop() {
 		case <-bp.dt.C:
 			successful := atomic.SwapInt64(&bp.successful, 0)
 			congested := atomic.SwapInt64(&bp.congested, 0)
+			denied := atomic.SwapInt64(&bp.denied, 0)
 			max := atomic.LoadInt64(&bp.max)
 
 			congestedPercent := float64(successful+congested) / 100 * float64(congested)
 
+			var incr, decr, same int64
 			switch {
 			case bp.cfg.Latency > 0 && bp.h.ValueAtPercentile(bp.cfg.LatencyPercentile) > bp.cfg.Latency.Nanoseconds():
 				bp.decr(max)
+				decr++
 			case congestedPercent >= bp.cfg.ThresholdPercent:
 				bp.decr(max)
+				decr++
 			case congestedPercent < bp.cfg.ThresholdPercent && congestedPercent > 0:
+				same++
 				// keep current max
 			default:
 				bp.incr(max)
+				incr++
 			}
+
+			bp.muxStats.Lock()
+			bp.stats = AIMDStats{
+				Max:                   atomic.LoadInt64(&bp.max),
+				MaxMax:                bp.cfg.MaxMax,
+				MaxMin:                bp.cfg.MinMax,
+				SuccessfulCounter:     bp.stats.SuccessfulCounter + successful,
+				CongestedCounter:      bp.stats.CongestedCounter + congested,
+				DeniedCounter:         bp.stats.DeniedCounter + denied,
+				DecideIncreaseCounter: bp.stats.DecideIncreaseCounter + incr,
+				DecideDecreaseCounter: bp.stats.DecideDecreaseCounter + decr,
+				DecideSameCounter:     bp.stats.DecideSameCounter + same,
+			}
+			bp.muxStats.Unlock()
 
 			bp.h.Reset()
 		case <-bp.closeCh:
@@ -141,11 +187,12 @@ func (bp *AIMD) decideLoop() {
 }
 
 func (bp *AIMD) incr(max int64) {
-	max = int64(math.Ceil(float64(max) * bp.cfg.IncreasePercent))
-	if max > bp.cfg.MaxMax {
-		max = bp.cfg.MaxMax
+	newMax := int64(math.Floor(float64(max)*bp.cfg.IncreasePercent) + 1)
+	if newMax > bp.cfg.MaxMax {
+		newMax = bp.cfg.MaxMax
 	}
-	atomic.StoreInt64(&bp.max, max)
+
+	atomic.StoreInt64(&bp.max, newMax)
 }
 
 func (bp *AIMD) decr(max int64) {
